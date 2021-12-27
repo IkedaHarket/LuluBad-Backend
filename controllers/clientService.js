da5fda52ab4668109c52f0b1c799e25d7dfafc45
Ikeda Harket <sebaaignacio111@gmail.com>
2021-12-27 11:43:33 -0300

    Client service CRUD 2/2 done

--- controllers/clientService.js
+++ controllers/clientService.js
@@ -73,8 +73,64 @@ const addClientService = async(req,res) =>{
     }
 }
 
+const modClientService = async(req,res) =>{
+    try {
+        const {id} = req.params;
+
+        if(verifyUserAdmin(req,res)) return false;
+
+        const {...data} = req.body;
+
+        const clientService = await ClientService.findByIdAndUpdate(id,{...data},{new:true}).populate([
+            { path: 'nail', model: 'Nail'},
+            { path: 'user', model: 'Usuario' },
+            { path: 'nailService', model: 'NailService' },
+        ])
+
+        return res.status(200).json({
+            ok:true,
+            clientService
+        })
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            msg:"Error interno del servidor",
+            desc:"Error controllers/clientService/modClientService"
+        })
+    }
+}
+
+const deleteClientService = async(req,res) =>{
+    try {
+        const {id} = req.params; 
+
+        if(verifyUserAdmin(req,res)) return false;
+
+        const clientService = await ClientService.findByIdAndDelete(id).populate([
+            { path: 'nail', model: 'Nail'},
+            { path: 'user', model: 'Usuario' },
+            { path: 'nailService', model: 'NailService' },
+        ])
+
+        return res.status(200).json({
+            ok:true,
+            clientService
+        })
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            msg:"Error interno del servidor",
+            desc:"Error controllers/clientService/deleteClientService"
+        })
+    }
+}
+
 module.exports = {
     getClientsServices,
     getClientService,
-    addClientService
+    addClientService,
+    modClientService,
+    deleteClientService
 }
--- routes/clientService.js
+++ routes/clientService.js
@@ -1,9 +1,9 @@
 const {Router} = require('express');
 const { check, param } = require('express-validator');
-const { addClientService, getClientsServices, getClientService } = require('../controllers/clientService');
+const { addClientService, getClientsServices, getClientService, modClientService, deleteClientService } = require('../controllers/clientService');
 
 const { verifyClientServiceId } = require('../helpers/verifyClientService');
-const { verifyNailServiceId, verifyNailId, verifyNailShapeId } = require('../helpers/verifyNails');
+const {  verifyNailId } = require('../helpers/verifyNails');
 
 const { verifyUserId } = require('../helpers/verifyUsers');
 
@@ -23,15 +23,33 @@ router.get('/:id',[
 
 router.post('/',[
     validarJWT,
-    // check('nailService').custom(verifyNailServiceId),
     check('nail','La uña es obligatoria').not().isEmpty(),
     check('nail').custom(verifyNailId),
-    // check('nailService').custom(verifyNailShapeId),
     check('user','El usuario es obligatorio').not().isEmpty(),
     check('user').custom(verifyUserId),
     check('total','El total es obligatorio').not().isEmpty(),
     validarCampos,
 ],addClientService)
+
+router.put('/:id',[
+    validarJWT,
+    param('id','El ID no puede estar vacio').not().isEmpty(),
+    param('id', 'No es un ID valido').isMongoId(),
+    param('id').custom(verifyClientServiceId),
+    check('nail','La uña es obligatoria').not().isEmpty(),
+    check('nail').custom(verifyNailId),
+    check('user','El usuario es obligatorio').not().isEmpty(),
+    check('user').custom(verifyUserId),
+    validarCampos
+],modClientService)
+
+router.delete('/:id',[
+    validarJWT,
+    param('id','El ID no puede estar vacio').not().isEmpty(),
+    param('id', 'No es un ID valido').isMongoId(),
+    param('id').custom(verifyClientServiceId),
+    validarCampos
+],deleteClientService)
 
 
 
